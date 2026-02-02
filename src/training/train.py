3ddf3b2f5ff8aa15a26577729dfa95f7538e348b
BrendxnW <BrendonHorse18@gmail.com>
2026-02-02 12:38:20 -0800

    reinstalled pytorch to switch to GPU

--- src/training/train.py
+++ src/training/train.py
@@ -1,7 +1,27 @@
 import torchvision.models as models
 import torch.nn as nn
+import torch.optim as optim
+from src.utils.utils import get_dataloaders
 
 
+
+train_loader, test_loader = get_dataloaders(batch_size=64, num_workers=2)
 
 resnet_model = models.resnet18(weights="IMAGENET1K_V1")
 feat_extract = nn.Sequential(*list(resnet_model.children())[:-1])
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(resnet_model.parameters(), lr=0.001)
+running_loss = 0.0
+
+for i, (image, caption) in enumerate(get_dataloaders.train_loader):
+    output = resnet_model(image)
+    loss = loss_function(output, caption)
+
+    print(f"Batch {i+1:5d} training loss: {loss.item():.4f}")
+
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+print("Finished Training")
